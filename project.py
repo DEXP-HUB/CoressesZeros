eda43be0fef0a99fa9bedb3b005b13e4deeaf513
Ali <ali_guseynov_02@mail.ru>
2024-08-17 15:01:46 +0300

    Dell index 0 in self.board

--- project.py
+++ project.py
@@ -1,46 +1,47 @@
 class Game:
 
-    def create_game(self):
+    def __create_game(self):
         self.player1 = 'x'
         self.player2 = '0'
         self.board = [' ', ' ', ' ',
                       ' ', ' ', ' ',
-                      ' ', ' ', ' ']
+                      ' ', ' ', ' ', ' ']
 
     def start_game(self):
-        self.create_game()
+        self.__create_game()
         for move in range(len(self.board)):
-            self.motion_players(self.player1)
-            if self.user_victory(self.player1):
+            self.__motion_players(self.player1)
+            if self.__user_victory(self.player1):
                 print('Победил X')
                 break
-            self.motion_players(self.player2)
-            if self.user_victory(self.player2):
+            self.__motion_players(self.player2)
+            if self.__user_victory(self.player2):
                 print('Победил 0')
                 break
 
-    def motion_players(self, player):
+    def __motion_players(self, player):
         while True:
             try:
                 print(f'Ходит {player}')
                 motion = int(input('Введите номер клетки: '))
-                if self.board[motion] == ' ':
+                if self.board[motion] == ' ' and motion > self.board.index(' ', 0) :
                     self.board[motion] = player
-                    print(self.board[0:3], self.board[3:6], self.board[6:9], sep='\n')
+                    print(self.board[1:4], self.board[4:7], self.board[7:10], sep='\n')
                     break
                 else:
-                    print('Эта ячейка уже занята.')
+                    print('Эта ячейка уже занята, или её не существует.')
             except (ValueError, IndexError):
                     print('Ошибка.')
 
-    def user_victory(self, player):
+    def __user_victory(self, player):
         lst = [player, player, player]
-        if (self.board[0:3] == lst or self.board[3:6] == lst or self.board[6:9] == lst) or \
-                (self.board[0:7:3] == lst or self.board[1:8:3] == lst or self.board[2:9:3] == lst) or \
-                (self.board[0:9:4] == lst or self.board[2:7:2] == lst):
+        if ((self.board[1:4] == lst or self.board[4:7] == lst or self.board[7:10] == lst) or
+                (self.board[1:8:3] == lst or self.board[2:9:3] == lst) or
+                (self.board[1:10:4] == lst or self.board[2:7:2] == lst)):
             return f'{player} Победил.'
 
 
 game = Game()
 game.start_game()
+
 
